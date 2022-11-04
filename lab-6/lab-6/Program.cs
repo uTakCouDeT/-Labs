a9e22aa12798e58426d850b5ca691b3c4286d087
uTakCouDeT <b20030021@gmail.com>
2022-11-04 19:41:47 +0300

    aboba

--- lab-6/lab-6/Program.cs
+++ lab-6/lab-6/Program.cs
@@ -1,3 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿public class Weather
+{
+    public string Country { set; get; }
+    public string Name { set; get; }
+    private string Temp { set; get; }
+    private string Description { set; get; }
 
-Console.WriteLine("Hello, World!");
+    public Weather(string contry, string name, string temp, string description)
+    {
+        Country = contry;
+        Name = name;
+        Temp = temp;
+        Description = description;
+    }
+    
+    public bool Equals(Weather other)
+    {
+        return Country == other.Country && Name == other.Name && Temp == other.Temp && Description == other.Description;
+    }
+    
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello");
+    }
+}
