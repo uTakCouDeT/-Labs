906da4ce84ae7b599e8c26353e8f6a5bed09d85c
uTakCouDeT <b20030021@gmail.com>
2022-11-04 20:03:10 +0300

    lab-6

--- lab-6/lab-6/Program.cs
+++ lab-6/lab-6/Program.cs
@@ -2,28 +2,33 @@
 {
     public string Country { set; get; }
     public string Name { set; get; }
-    private string Temp { set; get; }
+    private double Temp { set; get; }
     private string Description { set; get; }
 
-    public Weather(string contry, string name, string temp, string description)
+    public Weather(string contry, string name, double temp, string description)
     {
         Country = contry;
         Name = name;
         Temp = temp;
         Description = description;
     }
-    
+
+    public void Print()
+    {
+        Console.WriteLine($"{Country}, {Name}: {Temp}°C ({Description})");
+    }
+
     public bool Equals(Weather other)
     {
         return Country == other.Country && Name == other.Name && Temp == other.Temp && Description == other.Description;
     }
-    
 }
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello");
+        Weather weather = new Weather("Россия", "Москва", 7, "солнечно");
+        weather.Print();
     }
 }
