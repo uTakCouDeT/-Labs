b6776980f89e8ba43a35600bfdce087f0d04e3eb
uTakCouDeT <b20030021@gmail.com>
2022-11-10 06:29:16 +0300

    lab-7

--- lab-7/lab-7/Programm.cs
+++ lab-7/lab-7/Programm.cs
@@ -13,72 +13,30 @@ public class Program
         XDocument xDoc = new XDocument();
         XElement othr = new XElement(asm.GetName().Name);
         XElement clss = new XElement("Classes");
-
-        foreach (var type in asm.ExportedTypes)
-        {
-            if (type.IsAbstract)
-            {
-                Console.WriteLine("\nAbstract class:");
-            }
-            else if (type.IsClass)
-            {
-                Console.WriteLine("\nClass:");
-            }
-            else if (type.IsEnum)
-            {
-                Console.WriteLine("\nEnum:");
-            }
-
-            Console.WriteLine($" - {type.Name}");
-            Console.WriteLine("Properties: ");
-            foreach (var prop in type.GetProperties())
-            {
-                Console.WriteLine($" - {prop.Name}");
-            }
-
-            Console.WriteLine("Methods: ");
-            var methods = type.GetMethods();
-            foreach (var methodInfo in type.GetMethods())
-            {
-                Console.WriteLine($" - {methodInfo.Name}");
-            }
-        }
         
         foreach (var type in asm.ExportedTypes)
         {
-            var elemType = asm.GetType($"AnimalLib.{type.Name}");
-
-            if (type.IsAbstract)
+            Console.WriteLine(type.Name);
+            
+            if ((type.Name != "CommentAttribute") && type.IsClass)
             {
-                XElement abstrClass = new XElement($"{type.Name}");
-                clss.Add(abstrClass);
-            }
+                XElement element = new XElement($"{type.Name}");
 
-            if ((type.Name != "CommentAttribute") && type.IsClass && !type.IsAbstract)
-            {
-                XElement tempElement = new XElement($"{type.Name}");
-                var objectType = elemType.GetTypeInfo();
-
-
+                if (type.IsAbstract)
+                {
+                    XElement abstrClass = new XElement($"Abstract", " Abstract class ");
+                    element.Add(abstrClass);
+                }
+                
                 string commentsNames = "";
-                foreach (Attribute comment in objectType.GetCustomAttributes())
+                foreach (Attribute comment in type.GetTypeInfo().GetCustomAttributes())
                 {
                     if (comment is CommentAttribute animalComment)
                     {
                         commentsNames = animalComment.Comment;
                     }
                 }
-
                 XAttribute comments = new XAttribute("Attribute", commentsNames);
-                //XElement comments = new XElement("Attribute", commentsNames);
-
-                string fieldsNames = " ";
-                foreach (var classField in type.GetFields())
-                {
-                    fieldsNames += $"{classField.Name} ";
-                }
-
-                XElement fields = new XElement("Field", fieldsNames);
 
                 string propsNames = " ";
                 foreach (var property in type.GetProperties())
@@ -87,42 +45,37 @@ public class Program
                 }
 
                 XElement properties = new XElement("Proporties", propsNames);
-
-                string mathodsNames = " ";
+                
+                string methodsNames = " ";
                 foreach (var method in type.GetMethods())
                 {
-                    if (method.Name.Substring(0, 3) != "get" && method.Name.Substring(0, 3) != "set")
+                    if (method.Name.Substring(0, 4) != "get_" && method.Name.Substring(0, 4) != "set_" &&
+                        method.Name != "Equals" && method.Name != "GetHashCode" && method.Name != "GetType" &&
+                        method.Name != "ToString")
                     {
-                        if (method.Name != "Equals" && method.Name != "GetHashCode" && method.Name != "GetType" &&
-                            method.Name != "ToString")
-                        {
-                            mathodsNames += $"{method.Name} ";
-                        }
-                        //mathodsNames += $"{method.Name} ";
+                        methodsNames += $"{method.Name} ";
                     }
                 }
+                XElement methods = new XElement("Methods", methodsNames);
 
-                XElement methods = new XElement("Methods", mathodsNames);
 
-
-                tempElement.Add(comments);
-                tempElement.Add(fields);
-                tempElement.Add(properties);
-                tempElement.Add(methods);
-                clss.Add(tempElement);
+                element.Add(comments);
+                element.Add(properties);
+                element.Add(methods);
+                clss.Add(element);
             }
 
             if (type.IsEnum)
             {
-                XElement allEnums = new XElement($"{type.Name}");
+                XElement element = new XElement($"{type.Name}");
                 string enums = " ";
                 foreach (var enumElem in type.GetEnumNames())
                 {
                     enums += $"{enumElem} ";
                 }
-
-                allEnums.Add(enums);
-                othr.Add(allEnums);
+                
+                element.Add(enums);
+                othr.Add(element);
             }
         }
 
